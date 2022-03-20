fbdc0eb902148092c0f748102a335c557de5c723
FranciscoDias87 <franciscocpddias@hotmail.com>
2022-03-20 09:14:22 -0300

    atualizando favicon e criando a arquivo de pedidos

--- Web/cardapio/src/index.js
+++ Web/cardapio/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Pedidos from './cardapio/pedidos';
 
 ReactDOM.render(  
-   <h1>Teste</h1>,   
+     <Pedidos/>,   
   document.getElementById('root')
 );
 
