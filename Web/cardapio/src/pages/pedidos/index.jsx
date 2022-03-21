ea08f52442704a6133697d1e058c980fd2618d20
FranciscoDias87 <franciscocpddias@hotmail.com>
2022-03-21 19:28:21 -0300

    criando pasta service e arquivo api.js

--- Web/cardapio/src/pages/pedidos/index.jsx
+++ Web/cardapio/src/pages/pedidos/index.jsx
@@ -89,6 +89,7 @@ function Pedidos() {
                 endereco={item.endereco}
                 item={item.itens}              
               />
+              
             })            
           } 
             
